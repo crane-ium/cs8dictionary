ea2d23709dca59045e71eb4d9e757be95d0e6e6f
Stephen C <cranegames@gmail.com>
2018-10-20 16:47:18 -0700

    Updated tests

--- main.cpp
+++ main.cpp
@@ -2,7 +2,9 @@
 #include "dictionary.h"
 #include "../prime_generator/prime_generator.h"
 #include "doublehash.h"
-//#include
+#include "Windows.h"
+#include <random>
+#include <time.h>
 
 using namespace std;
 
@@ -47,7 +49,7 @@ int main()
     dh2.insert(3, "3");
     dh2.insert(200, "two-hundred");
     dh2.insert(150, "150");
-    dh2.insert(55, "OVERFLOW");
+//    dh2.insert(55, "OVERFLOW");
     cout << dh2 << endl;
     cout << (int)dh2 << endl;
     cout << dh2[200] << endl;
@@ -59,6 +61,72 @@ int main()
         cout << "Oops\n" << e.what()<<endl;
     }
 
+    //---- TIME TEST ----
+    //https://stackoverflow.com/questions/14337278/precise-time-measurement
+    LARGE_INTEGER frequency;
+    LARGE_INTEGER t_start, t_end;
+    double elapsed;
+
+    size_t prime1 = gen.get_prime(15000);
+    size_t prime2 = gen.get_prime(prime1-1);
+//    size_t prime3 = gen.get_prime(150);
+    size_t iterations = 10000;
+    size_t* randkeys = new size_t[iterations];
+    srand(time(NULL)); //comment out to make it repeatable tests
+    for(size_t i = 0; i < iterations; i++)
+        randkeys[i] = rand();
+    Dictionary<int,string> open_test(prime1);
+    DoubleHash<int,string> double_test(prime1, prime2);
+    //TEST OPEN HASH
+    QueryPerformanceFrequency(&frequency);
+    QueryPerformanceCounter(&t_start);
+    for(size_t i = 0; i < 10000; i++){
+        open_test.insert(randkeys[i], to_string(i));
+    }
+    QueryPerformanceCounter(&t_end);
+    elapsed = (t_end.QuadPart - t_start.QuadPart) * 1000.0 / frequency.QuadPart;
+    cout << "Open hash took " << elapsed << " milliseconds\n";
+    //TEST INSERT DOUBLE HASH
+    QueryPerformanceCounter(&t_start);
+    for(size_t i = 0; i < 10000; i++){
+        double_test.insert(randkeys[i], to_string(i));
+    }
+    QueryPerformanceCounter(&t_end);
+    elapsed = (t_end.QuadPart - t_start.QuadPart) * 1000.0 / frequency.QuadPart;
+    cout << "Double hash took " << elapsed << " milliseconds\n";
+    //TEST FIND OPEN HASH
+    QueryPerformanceCounter(&t_start);
+    for(size_t i = 0; i < 10000; i++){
+        open_test.find(randkeys[i]);
+    }
+    QueryPerformanceCounter(&t_end);
+    elapsed = (t_end.QuadPart - t_start.QuadPart) * 1000.0 / frequency.QuadPart;
+    cout << "Open find took " << elapsed << " milliseconds\n";
+    //TEST FIND DOUBLE HASH
+    QueryPerformanceCounter(&t_start);
+    for(size_t i = 0; i < 10000; i++){
+        double_test.find(randkeys[i]);
+    }
+    QueryPerformanceCounter(&t_end);
+    elapsed = (t_end.QuadPart - t_start.QuadPart) * 1000.0 / frequency.QuadPart;
+    cout << "Double find took " << elapsed << " milliseconds\n";
+
+    //Test indexing
+    DoubleHash<int,string> dh4(11);
+    dh4[1] = "Test";
+    dh4[100] = "test2";
+    dh4[1] = "Retest";
+    dh4[3] = "no dupe";
+    dh4[4];
+    cout << dh4;
 
     return 0;
 }
+
+
+
+
+
+
+
+
