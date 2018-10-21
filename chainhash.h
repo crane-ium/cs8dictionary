f10ddb7fd123d7f1237b5e83816c612c1b048aee
Stephen C <cranegames@gmail.com>
2018-10-20 18:59:42 -0700

    First commit

---
+++ chainhash.h
@@ -0,0 +1,10 @@
+#ifndef CHAINHASH_H
+#define CHAINHASH_H
+/* ChainHash:
+ * -Stores data in a hashtable where the table holds linked lists of records
+ * */
+
+
+
+
+#endif // CHAINHASH_H
---
+++ dict.h
@@ -0,0 +1,9 @@
+#ifndef DICT_H
+#define DICT_H
+/* Dict:
+ * -Stores keys of type size_t and string with assigned data
+ * */
+
+
+
+#endif // DICT_H
